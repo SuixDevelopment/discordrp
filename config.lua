5b583ad32861429ef53ee39da63e262f691d69ec
SuixDevelopment <100239167+SuixDevelopment@users.noreply.github.com>
2022-02-27 21:42:34 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.AppID = 940689686551212112
+Config.ImageAsset = 'logo'
+Config.ImageAssetText = ''
+Config.ImageAssetSmall = ''
+Config.ImageAssetText = ''
+
+Config.Buttons = {
+    {index = 0,name = 'Suix Development',url = 'https://discord.gg/rsRdPRfXUc'}
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'Reeve#8883'
+description 'suix_discordrp - join our discord server https://discord.gg/rsRdPRfXUc'
+
+shared_script 'config.lua'
+client_script 'src/client.lua'
